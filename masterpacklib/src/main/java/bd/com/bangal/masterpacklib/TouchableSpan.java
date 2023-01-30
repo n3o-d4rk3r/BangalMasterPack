fccff3f4cb205c9adf8981365ac568d76ede9a43
KABIR <md.kabirhassan2009@gmail.com>
2023-01-31 01:15:31 +0600

    Initial commit

---
+++ masterpacklib/src/main/java/bd/com/bangal/masterpacklib/TouchableSpan.java
@@ -0,0 +1,52 @@
+package bd.com.bangal.masterpacklib;
+
+
+import android.graphics.Color;
+import android.text.TextPaint;
+import android.text.style.ClickableSpan;
+
+/**
+ * Copyright © 2022 Aryan Kabir
+ *
+ * This subclass of {@link ClickableSpan} will change the text's color when touched. It also
+ * gives us the ability to underline the text as well.
+ *
+ * This works by utilizing a 'pressed' flag. When the flag is true, the span will draw the
+ * touched color, and when the flag is false, the span will draw the normal color.
+ *
+ * To amend the 'pressed' flag, call: {@link #setPressed(boolean)}.
+ *
+ * @author Aryan Kabir
+ * @version 1.0
+ */
+public abstract class TouchableSpan extends ClickableSpan {
+    private final boolean underlineEnabled;
+    private final int pressedTextColor;
+    private final int normalTextColor;
+    private boolean pressed;
+
+
+    public TouchableSpan(int normalTextColor, int pressedTextColor, boolean underlineEnabled) {
+        this.normalTextColor = normalTextColor;
+        this.pressedTextColor = pressedTextColor;
+        this.underlineEnabled = underlineEnabled;
+    }
+
+    @Override
+    public void updateDrawState(TextPaint paint) {
+        // Determine whether to paint it pressed or normally
+        int textColor = pressed ? pressedTextColor : normalTextColor;
+        paint.setColor(textColor);
+        paint.setUnderlineText(underlineEnabled);
+        paint.bgColor = Color.TRANSPARENT;
+    }
+
+    /**
+     * Sets the flag for when the span is pressed.
+     * @param value True if pressed
+     */
+    void setPressed(boolean value) {
+        this.pressed = value;
+    }
+}
+
---
+++ masterpacklib/src/main/java/bd/com/bangal/masterpacklib/URLEmbeddedTask.java
@@ -0,0 +1,70 @@
+package bd.com.bangal.masterpacklib;
+
+import android.os.AsyncTask;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+import java.net.URL;
+
+public class URLEmbeddedTask extends AsyncTask<String, Void, URLEmbeddedData> {
+
+    private OnLoadURLListener listener;
+
+    public URLEmbeddedTask(OnLoadURLListener listener) {
+        this.listener = listener;
+    }
+
+    @Override
+    protected URLEmbeddedData doInBackground(String... params) {
+        URLEmbeddedData data = new URLEmbeddedData();
+        try {
+            String url = params[0];
+            url = ((url.startsWith(URLConstants.PROTOCOL) || url.startsWith(URLConstants.PROTOCOL_S)) ? "" : URLConstants.PROTOCOL) + url;
+
+            URL host = new URL(url);
+            data.setHost(host.getHost());
+
+            Document doc = Jsoup.connect(url).get();
+            Elements elements = doc.select("meta");
+            for (Element e : elements) {
+                String tag = e.attr("property").toLowerCase();
+                String content = e.attr("content");
+                switch (tag) {
+                    case "og:url":
+                        URL urlNew = new URL(content);
+                        data.setHost(urlNew.getHost());
+                        break;
+                    case "og:image":
+                        data.setThumbnailURL(content);
+                        break;
+                    case "og:title":
+                        data.setTitle(content);
+                        break;
+                    case "og:description":
+                        data.setDescription(content);
+                        break;
+                }
+            }
+            data.setFavorURL(URLConstants.ROOT_URL_FAVOR_ICON + data.getHost());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    @Override
+    protected void onPostExecute(URLEmbeddedData result) {
+        if (listener != null) {
+            listener.onLoadURLCompleted(result);
+        }
+    }
+
+    public interface OnLoadURLListener {
+        void onLoadURLCompleted(URLEmbeddedData data);
+    }
+}
+
