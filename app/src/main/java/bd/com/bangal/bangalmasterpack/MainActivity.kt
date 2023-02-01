5327d02f1fa8c7ecf76aa1b58464f8c7847aff58
KABIR <md.kabirhassan2009@gmail.com>
2023-02-01 18:36:10 +0600

    Update

--- app/src/main/java/bd/com/bangal/bangalmasterpack/MainActivity.kt
+++ app/src/main/java/bd/com/bangal/bangalmasterpack/MainActivity.kt
@@ -1,14 +1,43 @@
-package bd.com.bangal.bangalmasterpack;
+package bd.com.bangal.bangalmasterpack
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle
+import android.os.Handler
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import bd.com.bangal.masterpacklib.CameraVideoButton
 
-import android.os.Bundle;
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-public class MainActivity extends AppCompatActivity {
+        val videoRecordButton = findViewById<CameraVideoButton>(R.id.button)
+        videoRecordButton.setVideoDuration(10000)
+        videoRecordButton.actionListener = object : CameraVideoButton.ActionListener {
+            override fun onCancelled() {
+                Log.v("TEST", "onCancelled")
+            }
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+            override fun onStartRecord() {
+                Log.v("TEST", "onStartRecord")
+            }
+
+            override fun onEndRecord() {
+                Log.v("TEST", "onEndRecord")
+            }
+
+            override fun onDurationTooShortError() {
+                Log.v("TEST", "onDurationTooShortError")
+            }
+
+            override fun onSingleTap() {
+                Log.v("TEST", "onSingleTap")
+            }
+
+        }
+
+        Handler().postDelayed(Runnable {
+            videoRecordButton.cancelRecording()
+        }, 5000)
     }
 }
