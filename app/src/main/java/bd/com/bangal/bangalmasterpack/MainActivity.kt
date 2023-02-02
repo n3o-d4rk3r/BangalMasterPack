4e65531af5420da2097667df75e0be3ab6dd975b
KABIR <md.kabirhassan2009@gmail.com>
2023-02-03 00:24:36 +0600

    Update

--- app/src/main/java/bd/com/bangal/bangalmasterpack/MainActivity.kt
+++ app/src/main/java/bd/com/bangal/bangalmasterpack/MainActivity.kt
@@ -5,8 +5,12 @@ import android.os.Handler
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import bd.com.bangal.masterpacklib.CameraVideoButton
+import bd.com.bangal.masterpacklib.ReactionPopup
+import bd.com.bangal.masterpacklib.ReactionsConfigBuilder
 
 class MainActivity : AppCompatActivity() {
+
+    private val strings = arrayOf("wow")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
