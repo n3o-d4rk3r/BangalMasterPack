7c621befff0ef2a19db154c55db09329af467991
KABIR <md.kabirhassan2009@gmail.com>
2023-02-21 18:04:13 +0600

    Update

--- app/src/main/java/bd/com/bangal/bangalmasterpack/TestActivity.java
+++ app/src/main/java/bd/com/bangal/bangalmasterpack/TestActivity.java
@@ -6,11 +6,9 @@ import android.os.Bundle;
 
 import bd.com.bangal.masterpacklib.ReactionPopup;
 import bd.com.bangal.masterpacklib.ReactionsConfigBuilder;
-import bd.com.bangal.socialviewcore.widget.SocialEditText;
 
 public class TestActivity extends AppCompatActivity {
 
-    SocialEditText bg_text,postText;
 
     private final String[] strings = {"wow"};
 
