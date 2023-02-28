a53105ad86389d7e6cb5fb4f5e3cbe2c0396e294
KABIR <md.kabirhassan2009@gmail.com>
2023-03-01 03:10:39 +0600

    Toast

--- app/src/main/java/bd/com/bangal/bangalmasterpack/MainActivity.kt
+++ app/src/main/java/bd/com/bangal/bangalmasterpack/MainActivity.kt
@@ -3,17 +3,62 @@ package bd.com.bangal.bangalmasterpack
 import android.os.Bundle
 import android.os.Handler
 import android.util.Log
+import android.view.View
+import android.widget.CompoundButton
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.res.ResourcesCompat
+import bd.com.bangal.bangalmasterpack.databinding.ActivityMainBinding
 import bd.com.bangal.masterpacklib.CameraVideoButton
 import bd.com.bangal.masterpacklib.ReactionPopup
 import bd.com.bangal.masterpacklib.ReactionsConfigBuilder
+import bd.com.bangal.masterpacklib.toast.MotionToast
+import bd.com.bangal.masterpacklib.toast.MotionToastStyle
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener,
+    CompoundButton.OnCheckedChangeListener {
 
+    private lateinit var binding: ActivityMainBinding
     private val strings = arrayOf("wow")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.successBtn.setOnClickListener(this)
 
     }
+
+        private fun setToastColors(newColorsEnabled: Boolean) {
+        if (newColorsEnabled) {
+            MotionToast.setSuccessBackgroundColor(R.color.success_bg_color)
+            MotionToast.setErrorBackgroundColor(R.color.error_bg_color)
+            MotionToast.setDeleteBackgroundColor(R.color.delete_bg_color)
+            MotionToast.setWarningBackgroundColor(R.color.warning_bg_color)
+            MotionToast.setInfoBackgroundColor(R.color.info_bg_color)
+        } else {
+            MotionToast.resetToastColors()
+        }
+    }
+
+        override fun onClick(v: View?) {
+        when (v!!.id) {
+            R.id.successBtn -> {
+                MotionToast.createColorToast(
+                    this,
+                    "Post create 😍",
+                    "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
+                    MotionToastStyle.SUCCESS,
+                    MotionToast.GRAVITY_BOTTOM,
+                    MotionToast.LONG_DURATION,
+                    ResourcesCompat.getFont(this, R.font.futurabold)
+                )
+            }
+        }
+    }
+
+        override fun onCheckedChanged(buttonView: CompoundButton?, isChecked: Boolean) {
+        setToastColors(isChecked)
+    }
+
 }
---
+++ masterpacklib/src/main/java/bd/com/bangal/masterpacklib/toast/MotionToastStyle.kt
@@ -0,0 +1,14 @@
+package bd.com.bangal.masterpacklib.toast
+
+import java.util.*
+
+enum class MotionToastStyle {
+    SUCCESS, ERROR, WARNING, INFO, DELETE, NO_INTERNET;
+
+    fun getName(): String {
+        if (this.name.contains("_")) {
+            return this.name.replaceFirst("_", " ")
+        }
+        return this.name
+    }
+}
